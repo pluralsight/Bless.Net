cbbf7bc19a13df838d077149cd7822cc404525fa
Jon <jonathan-turner@pluralsight.com>
2014-04-22 17:17:06 -0600

    Started parsing CSS

---
+++ Bless.Core.Tests/BlessParserTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Bless.Core.Tests
+{
+    [TestFixture]
+    public class BlessParserTests
+    {
+        private List<string> files;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var blessParser = new BlessParser();
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Bless.Core.Tests.example.css");
+            files = blessParser.Parse(stream);
+        }
+
+        [Test]
+        public void SplitsFiles()
+        {
+            Assert.That(files.Count, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void CheckFirstLinesOfFiles()
+        {
+            Assert.That(files[0].StartsWith("#test{background-color:red;}"));
+            Assert.That(files[1].StartsWith("#test{background-color:green;}"));
+            Assert.That(files[2].StartsWith("#test{background-color:blue;}"));
+            Assert.That(files[3].EndsWith("#test{background-color:white;}"));
+        }
+    }
+}
---
+++ Bless.Core/BlessParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ExCSS;
+
+namespace Bless.Core
+{
+    public class BlessParser
+    {
+        public static int MaxSelectorCount = 4095;
+
+        public List<string> Parse(Stream stream)
+        {
+            var files = new List<string>();
+            var parser = new Parser();
+            var stylesheet = parser.Parse(stream);
+
+            var file = new StringBuilder();
+            var index = 0;
+            foreach (StyleRule rule in stylesheet.Rulesets)
+            {
+                if (index >= MaxSelectorCount)
+                {
+                    index = 0;
+                    files.Add(file.ToString());
+                    file = new StringBuilder();
+                }
+
+                file.Append(rule);
+                index++;
+            }
+            files.Add(file.ToString());
+
+            return files;
+        }
+    }
+}
